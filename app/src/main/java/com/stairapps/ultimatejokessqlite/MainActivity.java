f597d9678f627feff93677f027d9dcd9a5bb6f06
Tepes Filip <tepes.filip@outlook.com>
2015-07-13 13:50:55 +0300

    implemented a new navigation drawer based on MaterialDrawer library

--- app/src/main/java/com/stairapps/ultimatejokessqlite/MainActivity.java
+++ app/src/main/java/com/stairapps/ultimatejokessqlite/MainActivity.java
@@ -7,11 +7,21 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import java.io.IOException;
 
@@ -20,14 +30,18 @@ public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
     private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
-    private SQLiteDatabase jokesDB;
+    private DataBaseHelper myDBHelper;
+    private Drawer result = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUp();
+        setUp(savedInstanceState);
         databaseSetUp();
 
+        
 
 
 
@@ -36,7 +50,7 @@ public class MainActivity extends ActionBarActivity {
 
     public void databaseSetUp(){
 
-        DataBaseHelper myDBHelper = new DataBaseHelper(this);
+        myDBHelper = new DataBaseHelper(this);
         try{
             myDBHelper.createDataBase();
         } catch (IOException e) {
@@ -48,10 +62,10 @@ public class MainActivity extends ActionBarActivity {
             throw sqle;
         }
 
-        jokesDB = myDBHelper.getDataBase();
+
     }
 
-    public void setUp(){
+    public void setUp(Bundle state){
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
@@ -59,9 +73,46 @@ public class MainActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+         result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withFullscreen(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Jokes").withIdentifier(0),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Pictures").withIdentifier(1),
+                        new PrimaryDrawerItem().withName("Favorites").withIdentifier(2),
+                        new PrimaryDrawerItem().withName("Settings").withIdentifier(3)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                        switch (drawerItem.getIdentifier()) {
+                            case 0:
+                                Toast.makeText(getApplicationContext(), "Jokes", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1:
+                                Toast.makeText(getApplicationContext(), "Pictures", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 2:
+                                Toast.makeText(getApplicationContext(), "Favorites", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 3:
+                                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                Toast.makeText(getApplicationContext(), "App is bugged", Toast.LENGTH_SHORT).show();
+                                break;
 
-        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+                        }
+                        return true;
+                    }
+                })
+                .withShowDrawerOnFirstLaunch(true)
+                .withSavedInstance(state)
+                .build();
+
+
     }
 
     @Override
@@ -69,13 +120,21 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState = result.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
 
-
-
-
-
-
-
+    @Override
+    public void onBackPressed() {
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
